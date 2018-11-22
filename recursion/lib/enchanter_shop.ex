6e85364e07b3f8379a460f68aaf5842ed858f566
dannash100 <danster15@gmail.com>
2018-11-22 14:06:39 +1300

    enchanter_shop

--- recursion/lib/enchanter_shop.ex
+++ recursion/lib/enchanter_shop.ex
@@ -1,8 +1,27 @@
-def test_data do
-  [
-    %{title: "Longsword", price: 50, magic: false},
-    %{title: "Healing Potion", price: 60, magic: true},
-    %{title: "Rope", price: 10, magic: false},
-    %{title: "Dragon's Spear", price: 100, magic: true},
-  ]
+defmodule EnchanterShop do
+
+  def test_data do
+    [
+      %{title: "Longsword", price: 50, magic: false},
+      %{title: "Healing Potion", price: 60, magic: true},
+      %{title: "Rope", price: 10, magic: false},
+      %{title: "Dragon's Spear", price: 100, magic: true},
+    ]
+  end
+
+  # @ symbol indicates module attributes - which can be used as constants
+
+  @enchanter_name "Edwin"
+
+  def enchant_for_sale([]), do: []
+  def enchant_for_sale([item | incoming_items]) do
+    new_item = %{
+      title: "#{@enchanter_name}'s #{item.title}",
+      price: item.price * 3,
+      magic: true
+    }
+
+    [new_item | enchant_for_sale(incoming_items)]
+  end
+
 end
