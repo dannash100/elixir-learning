32ab688dbe5ffdef6b06bf443df84bc2c1b8578e
dannash100 <danster15@gmail.com>
2018-11-25 13:45:56 +1300

    TailRecursion exercise

---
+++ recursion/lib/exercises/tail_recursion.ex
@@ -0,0 +1,9 @@
+defmodule TailRecursion do
+    def add_up(n), do: up_to(n, 0)
+    defp up_to(0, acc), do: acc
+    defp up_to(n, acc), do: n + up_to(n - 1, n + acc)
+
+    def sum(n), do: sum_of(n, 0)
+    def sum_of([], acc), do: acc
+    def sum_of([head | tail], acc), do: sum_of(tail, head + acc)
+end
